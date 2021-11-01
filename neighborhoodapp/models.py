24f2e3f2b4b8d7d05a3ca3555c0a6308cf3fc827
michael otieno <michael.otieno@student.moringaschool.com>
2021-11-01 20:54:44 +0300

    add neighborhood class

--- neighborhoodapp/models.py
+++ neighborhoodapp/models.py
@@ -1,3 +1,15 @@
+from os import name
 from django.db import models
+from django.db.models import ImageField
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    health_tel = models.IntegerField(null=True,blank=True)
+    police_number = models.IntegerField(null=True,blank=True)
+    family_count = models.IntegerField(null=True,blank=True)
+
+
