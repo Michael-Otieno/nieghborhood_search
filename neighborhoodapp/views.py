2a1fef59a22cc940e24ab2c2d6921e662648ac73
michael otieno <michael.otieno@student.moringaschool.com>
2021-11-02 06:48:17 +0300

    add hoods view function

--- neighborhoodapp/views.py
+++ neighborhoodapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponseRedirect
+
+from neighborhoodapp.models import NeighbourHood
 from .forms import SignupForm,Business,UpdateProfileForm,NeighbourHoodForm,PostForm
 from django.contrib.auth import login,authenticate
 from django.contrib.auth.decorators import login_required
@@ -23,3 +25,11 @@ def signup(request):
     else:
         form=SignupForm()
     return render(request,'registration.html', {'form':form})
+
+def hoods(request):
+    all_hoods = NeighbourHood.objects.all()
+    all_hoods=all_hoods[::-1]
+    params = {
+        'all_hoods':all_hoods,
+    }
+    return render(request, 'all_hoods.html', params)
