b3cc5fafee26398de9275f7e061f2f40a2fc8ef5
michael otieno <michael.otieno@student.moringaschool.com>
2021-11-02 06:16:50 +0300

    add signup form

---
+++ neighborhoodapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import models
+from django.forms import fields
+from .models import Profile,NeighbourHood,Business,Post
+from pyuploadcare.dj.forms import ImageField
+
+class SignupForm(UserCreationForm):
+    email=forms.EmailField(max_length=254, help_text='Requred.Inform a valid email address')
+    class Meta:
+        models=User
+        fields=('username','email','password1','password2')
